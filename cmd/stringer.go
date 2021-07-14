33e4547902d2f58bb1c1a51d4992d2ff8c590afe
czy <czy463@163.com>
2021-07-14 12:49:36 +0800

    cobra init command

---
+++ cmd/stringer.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2021 chenzhiyuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// stringerCmd represents the stringer command
+var stringerCmd = &cobra.Command{
+	Use:   "stringer",
+	Short: "Stringer automate create fmt.Stringer interface",
+	Long: `copy from https://github.com/golang/tool/cmd/stringer.
+Copyright 2014 The Go Authors. All rights reserved.
+
+Stringer is a tool to automate the creation of methods that satisfy the fmt.Stringer
+interface. Given the name of a (signed or unsigned) integer type T that has constants
+defined, stringer will create a new self-contained Go source file implementing
+	func (t T) String() string
+The file is created in the same package and directory as the package that defines T.
+It has helpful defaults designed for use with go generate.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("stringer called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stringerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// stringerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// stringerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
